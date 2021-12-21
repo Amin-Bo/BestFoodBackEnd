89c51c83105439fa9e03284d1c6791a3049d51d5
dali2000 <medalikebair@gmail.com>
2021-12-21 18:52:49 +0100

    adding cors

--- server.js
+++ server.js
@@ -3,12 +3,18 @@ const express = require('express');
 const Mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 var passport = require('passport')
+var cors = require('cors')
+
 // Initialize App 
 const app = express();
 const userRoutes = require('./Routes/users');
 // DB connection
 //-----------------------------------------------//
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+ next();
+});
 Mongoose.connect(process.env.DATABASE, { useNewUrlParser: true, useUnifiedTopology: true }, {
 
 });
